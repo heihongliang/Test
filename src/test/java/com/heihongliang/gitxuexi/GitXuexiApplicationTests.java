9d2bd6ae2f19e165219e75057283c573b2dfbbd7
heihongliang <1607763141@qq.com>
2020-10-18 21:25:46 +0800

    提交

---
+++ src/test/java/com/heihongliang/gitxuexi/GitXuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heihongliang.gitxuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
